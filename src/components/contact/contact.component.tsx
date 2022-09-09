07076a600001a61c03bbc7289ec7306fd07280a4
Callum Silcock <github@csi.lk>
2022-09-09 11:01:44 +1000

    feat(contact): add styled landing after form submission
    as per netlify docs

--- src/components/contact/contact.component.tsx
+++ src/components/contact/contact.component.tsx
@@ -2,7 +2,14 @@ import { h } from 'preact';
 import style from './contact.module.scss';
 
 export const Contact = () => (
-  <form name="contact" method="POST" data-netlify="true" class={style.contact}>
+  <form
+    name="contact"
+    method="POST"
+    data-netlify="true"
+    class={style.contact}
+    action="/contact-success/"
+    netlify-honeypot="anti-s"
+  >
     <fieldset>
       <input type="text" name="name" placeholder="Your Name*" required />
     </fieldset>
@@ -11,6 +18,9 @@ export const Contact = () => (
     </fieldset>
     <fieldset>
       <textarea name="message" placeholder="Message to the author" required />
+    </fieldset>
+    <fieldset class={style.hidden}>
+      Don't fill this out if you're human: <input name="anti-s" />
     </fieldset>
     <p>
       <button type="submit">Send Message</button>
